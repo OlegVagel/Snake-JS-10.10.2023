707d0e350fa8ac3a2ddd449b9d13220825f79e01
Oleg <oleg.vagel@gmx.de>
2023-10-10 15:23:37 +0200

    Initial Commit
    -complete html & Css

---
+++ script.js
@@ -0,0 +1,6 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.GetContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+let gameWidth = gameBoard.width;
+let gameHeight = gameBoard.height;
